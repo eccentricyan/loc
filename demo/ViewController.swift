b199faaac5380a1f84b4cc0471e06d710b9e7d62
eccentricyan <eccentricyan@gmail.com>
2020-09-03 09:14:19 +0900

    fix

---
+++ demo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by seki on 2020/09/03.
+//  Copyright © 2020 seki. All rights reserved.
+//
+
+import UIKit
+import loc
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .bondiBlue
+    }
+
+
+}
+
---
+++ loc.framework/Headers/loc.h
@@ -0,0 +1,19 @@
+//
+//  loc.h
+//  loc
+//
+//  Created by seki on 2020/09/03.
+//  Copyright © 2020 seki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for loc.
+FOUNDATION_EXPORT double locVersionNumber;
+
+//! Project version string for loc.
+FOUNDATION_EXPORT const unsigned char locVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <loc/PublicHeader.h>
+
+
